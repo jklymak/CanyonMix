41a1b4e1ee5f72e0d806d772f0ae40a5287ca9c4
Jody Klymak <jklymak@gmail.com>
2025-06-09 13:09:06 -0700

    gendata for Slope2D012: Enhanced near-bottom Kr, constant strat.  Constant slope, no forcing

--- input/gendata.py
+++ input/gendata.py
@@ -438,6 +438,7 @@ if True:
         _log.info("Using decay %f and strength %f", decay, strength)
         K = np.ones((nz, ny, nx)) * 1e-5
         for i in range(0, nx):
+            print(d[0, i])
             if d[0, i] > -H:
                 K[:, 0, i] = strength * np.exp(-(z+d[0, i]) / decay)
         fig, ax = plt.subplots()

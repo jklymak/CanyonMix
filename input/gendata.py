56dcfc86cd298b02ee942b29b376759abaf1a5ae
Jody Klymak <jklymak@gmail.com>
2025-08-01 14:08:55 -0700

    gendata for Slope2D023: Enhanced near-bottom Kr=4e-1 exponential 250m scale, larger horizontal K, exponential strat.  Constant slope, no forcing

--- input/gendata.py
+++ input/gendata.py
@@ -24,12 +24,12 @@ _log = logging.getLogger(__name__)
 
 
 if True:
-    runno = 22
+    runno = 23
     u0 = 0.0
     f0 = 0.0
     fixedKz = 'file'
     sourceKz = 'Slope2D002'
-    sourceKz = (250, 4e-1, True) # decay, strength, exponential
+    sourceKz = (250, 1e-1, True) # decay, strength, exponential
     geo_beta = 0.0
     strat_scale = 500 # 500  # m
     strat_scale_comp = 500

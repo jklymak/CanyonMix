30249721886a135c28b982ecf32fd310f70d58f9
Jody Klymak <jklymak@gmail.com>
2026-06-29 21:22:24 -0700

    gendata for Slope2D102: Like Slope2d091 multiple slopes; 0.3 m/s velocity.

--- input/gendata.py
+++ input/gendata.py
@@ -23,7 +23,7 @@ _log = logging.getLogger(__name__)
 
 
 if True:
-    runno = 101
+    runno = 102
     u0 = 0.3
     f0 = 0.0
     fixedKz = None
@@ -68,12 +68,15 @@ if True:
     sub = om / N0 * 0.5
     # Run 10 and 91:
     db = np.array([0., -400, -700-200, -1000-200, -1500-200, -1800-190, -2000])
+    xb = 0. * db
+    xb[1] = 15_000.
     crit = [0, sub, super,  sub, super, sub, super]
     # One slope:
-    db = np.array([0., -300, -1000, -2000])
-    xb = 0. * db
-    xb[1] = 25_000.
-    crit = [0, sub,  sub, sub]
+    if False:
+        db = np.array([0., -300, -1000, -2000])
+        xb = 0. * db
+        xb[1] = 25_000.
+        crit = [0, sub,  sub, sub]
 
     for td in range(2, len(db)):
         xb[td] = xb[td-1] + (db[td-1] - db[td]) / crit[td]
@@ -89,7 +92,7 @@ if True:
     #comments = f"{runname} alpha = {alpha}. {strattype} stratification. u_0={u0}. N_0={N0}.  Four tracers\n"
     #comments += f"   topox: {xb} topodepth: {db}\n"
     #print(comments)
-    comments = "Like Slope2d097 super critical slope; 0.3 m/s velocity.\n"
+    comments = "Like Slope2d091 multiple slopes; 0.3 m/s velocity.\n"
     _log.info("runname %s", runname)
     #_log.info("dhdx %f", dhdx)
 

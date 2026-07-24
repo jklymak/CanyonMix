745e9b912e0e07f5833484870f7b6975a3332afd
Jody Klymak <jklymak@gmail.com>
2026-07-23 22:38:15 -0700

    gendata for Slope2D304: Critical slope exponential strat; 0.3 m/s velocity.

--- input/gendata.py
+++ input/gendata.py
@@ -292,10 +292,10 @@ if True:
 
                 d[0, i] = d[0, i+1] - (dhdx * dx[i])
         # make a linear from X=-50 to the topography say about 250 m.
-        ind = np.where(xx>-55_000)[0][0]
+        ind = np.where(xx>-60_000)[0][0]
         print(ind)
         for i in range(ind, nx):
-            d[0, i] = np.max([-2000 + (xx[i] + 55_000) * 250 / 15_000, d[0, i]])
+            d[0, i] = np.max([-2000 + (xx[i] + 60_000) * 250 / 15_000, d[0, i]])
         # d[0, :] = np.convolve(d[0, :], np.ones(10) / 10, mode="same")
 
     if len(xb) > 2:
